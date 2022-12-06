4971971c4e3f55a07d6bc47b38a710503de6d1bd
johnplayer1982 <johnplayer1982@msn.com>
2022-12-06 09:25:54 +0000

    Removes unused imports

--- Lib/testauth.py
+++ Lib/testauth.py
@@ -1,6 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time, os
 from dotenv import load_dotenv
 load_dotenv()
